7f24ae3ad61c1d37150c1900b408bba2012c7e1f
imtiazahammed1 <imtiaz2015160042@gmail.com>
2022-03-26 21:05:54 +0600

    style-added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Product.css'
 
 const Product = (props) => {
     const { name, price, img } = props.product;
@@ -6,7 +7,7 @@ const Product = (props) => {
         <div className='product'>
             <img src={img} alt=""></img>
             <div className='product-info'>
-                <h3 className='product-name'>{name}</h3>
+                <h3>{name}</h3>
                 <p>Price: ${price}</p>
             </div>
         </div>
