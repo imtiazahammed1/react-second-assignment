6495be38889cb485e6e10c8b80eb9ccef00955c8
imtiazahammed1 <imtiaz2015160042@gmail.com>
2022-03-26 23:09:09 +0600

    cart-added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({ cart }) => {
+
+    return (
+        <div>
+            <h3 className='cart-info'>Selected Products</h3>
+            {
+                cart.map(item => <h5>{item.name}</h5>)
+            }
+            <button className='cart-btn1'>Choose 1 For Me</button>
+            <br></br>
+            <button className='cart-btn2'>Choose Again</button>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -14,9 +15,15 @@ const Shop = () => {
     }, []);
 
     const handleAddToCart = (product) => {
-        console.log(product);
         const newCart = [...cart, product];
         setCart(newCart);
+    }
+    const handleRandomCart = () => {
+        const randomNumber = Math.floor(Math.random() * cart.length);
+        setCart([cart[randomNumber]]);
+    }
+    const handleDeleteCart = () => {
+        setCart([]);
     }
 
     return (
@@ -32,8 +39,11 @@ const Shop = () => {
             </div>
 
             <div className='carts-container'>
-                <h2>Selected products</h2>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}
+                    handleRandomCart={handleRandomCart}
+                    handleDeleteCart={handleDeleteCart}
+                ></Cart>
+
             </div>
 
         </div>
